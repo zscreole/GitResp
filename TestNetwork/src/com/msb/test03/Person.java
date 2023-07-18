efb55cf234ae75d7ebb983a0a5f5d78796b1e4ba
adam <zscreole@qq.com>
2023-07-18 16:48:23 +0800

    我在Person类中增加了name属性

---
+++ TestNetwork/src/com/msb/test03/Person.java
@@ -0,0 +1,11 @@
+package com.msb.test03;
+
+/**
+ * @author: Adam
+ * @date: 2023/7/18 - 16:47
+ * @description: com.msb.test03
+ * @version: 1.0
+ */
+public class Person {
+    private String name;
+}
